52628f557b35252b49373dbe76bb0f48ab9d7b00
Aaron Hans <aaronhans@gmail.com>
2022-11-10 10:50:01 -0800

    add css and reporting fixes

--- src/index.js
+++ src/index.js
@@ -20,14 +20,9 @@ export class CaGovBenefitsRecs extends window.HTMLElement {
 
     this.html = html;
     this.css = css;
-    // staging
-    // this.benefitsAPI = 'https://7ksmy2xna5.execute-api.us-west-1.amazonaws.com/';
-    // production
     this.benefitsAPI = 'https://k61aw4mwkc.execute-api.us-west-1.amazonaws.com/'
   }
 
-  // respond to changed attributes
-  
   connectedCallback() {
     this.language = navigator.language;
     this.income = "";
@@ -56,54 +51,68 @@ export class CaGovBenefitsRecs extends window.HTMLElement {
     .then((response) => response.json())
     .then((data) => {
       let json = JSON.parse(data);
+      
+      // if i receive no info from the api do nothing
+      if(json.links && json.links.length > 1) {
 
-      // create template from imported html
-      let template = document.createElement('template');
-      template.innerHTML = this.html;
-      // with style tag
-      let style = document.createElement('style');
-      template.content.prepend(style);
-      // create shadow root
-      this.attachShadow({mode: "open"});
-      this.shadowRoot.append(template.content.cloneNode(true));
-      // insert css into style element in template in shadow root
-      this.shadowRoot.querySelector('style').append(this.css); 
 
-      this.shadowRoot.querySelector('h2').innerHTML = json.header;
-      this.shadowRoot.querySelector('p.tagline').innerHTML = json.tagline;
-      let listContainer = this.shadowRoot.querySelector('ul.benefits');
-      json.links.forEach(link => {
-        // the classnames used here are inside a shadow root so can be generic as they won't inherit rules applied to the same name outside this component's shadow DOM
-        listContainer.innerHTML += `
-          <li>
-            <a href="${link.url}">
-              <span class="details">
-                <span class="svg">${link.graphic}</span>
-                <span class="offer">
-                  <span class="linktext">${link.linktext}</span>
-                  <span class="description">${link.description}</span>
+        // create template from imported html
+        let template = document.createElement('template');
+        template.innerHTML = this.html;
+        // with style tag
+        let style = document.createElement('style');
+        template.content.prepend(style);
+        // create shadow root
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.append(template.content.cloneNode(true));
+        // insert css into style element in template in shadow root
+        this.shadowRoot.querySelector('style').append(this.css); 
+
+        this.shadowRoot.querySelector('h2').innerHTML = json.header;
+        this.shadowRoot.querySelector('p.tagline').innerHTML = json.tagline;
+        let listContainer = this.shadowRoot.querySelector('ul.benefits');
+        json.links.forEach(link => {
+          // the classnames used here are inside a shadow root so can be generic as they won't inherit rules applied to the same name outside this component's shadow DOM
+          listContainer.innerHTML += `
+            <li>
+              <a href="${link.url}">
+                <span class="details">
+                  <span class="svg">${link.graphic}</span>
+                  <span class="offer">
+                    <span class="linktext">${link.linktext}</span>
+                    <span class="description">${link.description}</span>
+                  </span>
                 </span>
-              </span>
-              <span class="program-caret">
-                <span class="program">
-                  ${link.program}
+                <span class="program-caret">
+                  <span class="program">
+                    ${link.program}
+                  </span>
+                  <span class="caret">
+                    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0.34999 11.147C0.64299 11.44 1.11799 11.44 1.40999 11.147L6.27399 6.28198C6.42099 6.13498 6.49499 5.94098 6.49299 5.74898C6.49299 5.55498 6.42099 5.36298 6.27399 5.21598L1.40999 0.350976C1.11699 0.0579756 0.64199 0.0579756 0.34999 0.350976C0.0579904 0.643976 0.0569904 1.11898 0.34999 1.41198L4.68699 5.74898L0.35099 10.086C0.0579898 10.379 0.0579898 10.854 0.35099 11.147H0.34999Z" fill="black"/>
+                    </svg>
+                  </span>
                 </span>
-                <span class="caret">
-                  <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.34999 11.147C0.64299 11.44 1.11799 11.44 1.40999 11.147L6.27399 6.28198C6.42099 6.13498 6.49499 5.94098 6.49299 5.74898C6.49299 5.55498 6.42099 5.36298 6.27399 5.21598L1.40999 0.350976C1.11699 0.0579756 0.64199 0.0579756 0.34999 0.350976C0.0579904 0.643976 0.0569904 1.11898 0.34999 1.41198L4.68699 5.74898L0.35099 10.086C0.0579898 10.379 0.0579898 10.854 0.35099 11.147H0.34999Z" fill="black"/>
-                  </svg>
-                </span>
-              </span>
-            </a>
-          </li>
-        `;
-      })
+              </a>
+            </li>
+          `;
+        })
 
-      // post event render
-      this.recordEvent('render');
-      // apply other listeners
-      this.applyListeners();
+        // post event render
+        this.recordEvent('render');
+        // apply other listeners
+        this.applyListeners();
+      } else {
+        // no links received from api, print comment on frontend
 
+        // create template from imported html
+        let template = document.createElement('template');
+        template.innerHTML = '<!-- benefits widget: no links to display received, probably intentionally throttling traffic -->';
+        // create shadow root
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.append(template.content.cloneNode(true));
+        
+      }
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -128,10 +137,6 @@ export class CaGovBenefitsRecs extends window.HTMLElement {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(this.widgetEnvData)
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
     });
   }
 
