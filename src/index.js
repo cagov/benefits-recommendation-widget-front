ca5d8351f60988daf0191a0aa43b89abde8f89bd
Jon Jensen <github@id.xjensen.com>
2023-08-15 12:45:00 -0700

    Add custom template support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import html from "./template.html.js";
-import css from "./index.css.js";
-import linkHtml from "./link.html.js";
+import { defaultHtml } from "./template.html.js";
 
 /**
  * Web component that retrieves and displays a list of benefits available to Californians that are recommended for the current visitor on the host site
@@ -17,10 +15,8 @@ import linkHtml from "./link.html.js";
  */
 export class CaGovBenefitsRecs extends window.HTMLElement {
   constructor() {
-    super(html, css);
+    super();
 
-    this.html = html;
-    this.css = css;
     this.benefitsAPI =
       "https://k61aw4mwkc.execute-api.us-west-1.amazonaws.com/";
   }
@@ -72,24 +68,49 @@ export class CaGovBenefitsRecs extends window.HTMLElement {
 
         // if i receive no info from the api do nothing
         if (data.links && data.links.length > 1) {
-          // create template from imported html
-          let template = document.createElement("template");
-          template.innerHTML = this.html;
-          // with style tag
-          let style = document.createElement("style");
-          template.content.prepend(style);
+          let template;
+
+          // If host supplies their own template, let's use it.
+          const customTemplateId = "cagov-benefits-recs-template";
+          const customTemplate = document.getElementById(customTemplateId);
+          if (customTemplate) template = customTemplate;
+
+          // Otherwise, use default template.
+          if (!template) {
+            template = document.createElement("template");
+            template.innerHTML = defaultHtml;
+          }
+
+          const h2 = template.content.querySelector("h2");
+          h2.innerHTML = data.header;
+
+          const tagline = template.content.querySelector("p.tagline");
+          tagline.innerHTML = data.tagline;
+
+          const linkList = template.content.querySelector(".link-list");
+          const linkItem = linkList.querySelector(".link-item");
+          linkItem.remove();
+
+          const props = ["graphic", "description", "linktext", "program"];
+
+          // Clone and populate the HTML for each link.
+          data.links.forEach((link) => {
+            const newLinkItem = linkItem.cloneNode(true);
+
+            props.forEach((prop) => {
+              const el = newLinkItem.querySelector(`.${prop}`);
+              if (el) el.innerHTML = link[prop];
+            });
+
+            const anchor = newLinkItem.querySelector(".anchor");
+            if (anchor) anchor.href = link.url;
+
+            linkList.append(newLinkItem);
+          });
+
           // create shadow root
           this.attachShadow({ mode: "open" });
           this.shadowRoot.append(template.content.cloneNode(true));
-          // insert css into style element in template in shadow root
-          this.shadowRoot.querySelector("style").append(this.css);
-
-          this.shadowRoot.querySelector("h2").innerHTML = data.header;
-          this.shadowRoot.querySelector("p.tagline").innerHTML = data.tagline;
-          let listContainer = this.shadowRoot.querySelector("ul.benefits");
-          data.links.forEach((link) => {
-            listContainer.innerHTML += linkHtml(link);
-          });
 
           this.widgetEnvData.experimentName = data.experimentName;
           this.widgetEnvData.experimentVariation = data.experimentVariation;
